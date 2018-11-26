2bc10acad979f20424396c34c4ae9f9ed6404eeb
Johan Swensson <johan@redhat.com>
2018-11-26 17:00:11 +0100

    change defaults

--- ocp-cluster/prov.sh
+++ ocp-cluster/prov.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-../install.sh master1 8192 | grep DONE
-../install.sh master2 8192 | grep DONE
-../install.sh master3 8192 | grep DONE
-../install.sh infra1 8192 | grep DONE
-../install.sh infra2 8192 | grep DONE
-../install.sh infra3 8192 | grep DONE
-../install.sh infra4 8192 | grep DONE
-../install.sh node1 4096 | grep DONE
-../install.sh node2 2048 | grep DONE
-../install.sh openshift 1024 | grep DONE
+../install.sh -n master1 -m 8192 -d 50 -e 15 | grep DONE
+../install.sh -n master2 -m 8192 -d 50 -e 15 | grep DONE
+../install.sh -n master3 -m 8192 -d 50 -e 15 | grep DONE
+../install.sh -n infra1 -m 8192 -d 50 -e 15,250 | grep DONE
+../install.sh -n infra2 -m 8192 -d 50 -e 15,250 | grep DONE
+../install.sh -n infra3 -m 8192 -d 50 -e 15,250 | grep DONE
+../install.sh -n infra4 -m 8192 -d 50 -e 15,250 | grep DONE
+../install.sh -n node1 -m 4096 -d 50 -e 15 | grep DONE
+../install.sh -n node2 -m 4096 -d 50 -e 15| grep DONE
+../install.sh -n openshift -m 1024 | grep DONE
+
